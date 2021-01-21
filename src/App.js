503ce64541cdb10b5f5bad5e1db51d8c7a17e896
cchechang <cchechang@mastercontrol.com>
2021-01-21 00:01:07 -0700

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import InitialPrice from './components/InitialPrice';
 import TipSelection from './components/TipSelection';
 import TipsTotal from './components/TipsTotal';
